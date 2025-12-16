cad8b8912d6952f8a2b39342cbd65bd441a36085
SuperSimpleDev <mhamadhemn840@gmail.com>
2025-12-16 19:25:36 +0300

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart=[{
+export let cart= JSON.parse(localStorage.getItem('cart'))
+
+if(!cart){
+ cart =
+ [{
   productId :'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
 
@@ -6,6 +10,8 @@ export let cart=[{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }]
+}
+
 
 
 
@@ -26,6 +32,12 @@ export function addToCart(productId, quantity) {
       quantity: quantity
     });
   }
+  saveToStorage();
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
 }
 
 
@@ -40,4 +52,6 @@ export function removeFromCart(productId){
   })
 
 cart =newCart
+
+saveToStorage();
 }
