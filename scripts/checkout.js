a85aff7ba8df80be43b9b795026bed314edd6f8e
SuperSimpleDev <mhamadhemn840@gmail.com>
2025-12-16 18:57:24 +0300

    fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartHTML = '';
 
@@ -29,7 +30,7 @@ cart.forEach((cartItem) => {
           </div>
 
           <div class="product-price">
-            $${matchingProduct.price}
+            $${formatCurrency(matchingProduct.priceCents)}
           </div>
 
           <div class="product-quantity">
@@ -41,6 +42,63 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">Update</span>
             <span class="delete-quantity-link link-primary">Delete</span>
           </div>
+        </div>
+
+        <!-- ✅ DELIVERY OPTIONS -->
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+
+          <div class="delivery-option">
+            <input
+              type="radio"
+              checked
+              class="delivery-option-input"
+              name="delivery-option-${cartItem.productId}"
+            >
+            <div>
+              <div class="delivery-option-date">
+                Tuesday, June 21
+              </div>
+              <div class="delivery-option-price">
+                FREE Shipping
+              </div>
+            </div>
+          </div>
+
+          <div class="delivery-option">
+            <input
+              type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${cartItem.productId}"
+            >
+            <div>
+              <div class="delivery-option-date">
+                Wednesday, June 15
+              </div>
+              <div class="delivery-option-price">
+                $4.99 - Shipping
+              </div>
+            </div>
+          </div>
+
+          <div class="delivery-option">
+            <input
+              type="radio"
+              class="delivery-option-input"
+              name="delivery-option-${cartItem.productId}"
+            >
+            <div>
+              <div class="delivery-option-date">
+                Monday, June 13
+              </div>
+              <div class="delivery-option-price">
+                $9.99 - Shipping
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
