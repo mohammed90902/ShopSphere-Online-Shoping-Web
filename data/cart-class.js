530e042361f7831004364b1415b7555730ab6e73
SuperSimpleDev <mhamadhemn840@gmail.com>
2025-12-19 21:29:06 +0300

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,55 @@
+class Cart {
+  items = [];
+
+  constructor(storageKey) {
+    this.storageKey = storageKey;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.items = JSON.parse(localStorage.getItem(this.storageKey)) || [];
+  }
+
+  saveToStorage() {
+    localStorage.setItem(
+      this.storageKey,
+      JSON.stringify(this.items)
+    );
+  }
+
+  addToCart(productId, quantity) {
+    const item = this.items.find(
+      item => item.productId === productId
+    );
+
+    if (item) {
+      item.quantity += quantity;
+    } else {
+      this.items.push({
+        productId,
+        quantity,
+        deliveryOptionId: '1'
+      });
+    }
+
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    this.items = this.items.filter(
+      item => item.productId !== productId
+    );
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    const item = this.items.find(
+      item => item.productId === productId
+    );
+
+    if (item) {
+      item.deliveryOptionId = String(deliveryOptionId);
+      this.saveToStorage();
+    }
+  }
+}
